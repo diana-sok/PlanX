5e16f24523dc7ebfacc799557b5956cf65199f55
diana sok <dianadesok98@gmail.com>
2019-06-26 01:17:37 -0700

    Initial Commit

---
+++ PlanX/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PlanX
+//
+//  Created by Diana Sok on 6/26/19.
+//  Copyright © 2019 H2OT. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PlanX/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PlanX
+//
+//  Created by Diana Sok on 6/26/19.
+//  Copyright © 2019 H2OT. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
