8914499a1dd059d5cfad02b36560a6f4215a592b
diana sok <dianadesok98@gmail.com>
2019-07-15 13:10:02 -0700

    Student for Singleton

---
+++ PlanX/Student/CourseDivision.swift
@@ -0,0 +1,15 @@
+//
+//  CourseDivision.swift
+//  PlanX
+//
+//  Created by Diana Sok on 7/15/19.
+//  Copyright © 2019 H2OT. All rights reserved.
+//
+
+//singleton pattern
+import Foundation
+
+class CourseDivision {
+    private var divisionType:String = "" //Test, Homework etc
+    private var percentOfWhole:Int = 0 //30% of course grade
+}
---
+++ PlanX/Student/Student.swift
@@ -0,0 +1,17 @@
+//
+//  Student.swift
+//  PlanX
+//
+//  Created by Diana Sok on 7/15/19.
+//  Copyright © 2019 H2OT. All rights reserved.
+//
+
+import Foundation
+
+class Student {
+    private var firstName:String = ""
+    private var lastName:String = ""
+    private var uid:String = ""
+    private var courses:[Course] = []
+    
+}
