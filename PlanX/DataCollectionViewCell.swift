8408f44ee551810cf9e88605b0c33e60770c2a66
diana sok <dianadesok98@gmail.com>
2019-07-15 17:32:49 -0700

    changed file location

---
+++ PlanX/DataCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DataCollectionViewCell.swift
+//  H2OT
+//
+//  Created by admin on 7/12/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class DataCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var DateLabel: UILabel!
+    
+}
