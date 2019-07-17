4a824cad48d029f03e04e19acf7c32acb723161e
diana sok <dianadesok98@gmail.com>
2019-07-17 01:15:16 -0700

    student, task, courses, courseDivision basics

--- PlanX/Student/CourseDivision.swift
+++ PlanX/Student/CourseDivision.swift
@@ -11,5 +11,59 @@ import Foundation
 
 class CourseDivision {
     private var divisionType:String = "" //Test, Homework etc
-    private var percentOfWhole:Int = 0 //30% of course grade
+    private var percentOfWhole:Double = 0 //30% of course grade //double or int
+    private var tasks:[Task] = []
+    
+    //Constructors
+    init(divisionType:String, percentOfWhole:Double) {
+        self.divisionType = divisionType
+        self.percentOfWhole = percentOfWhole
+        tasks = []
+    }
+    
+    init(divisionType:String, percentOfWhole:Double, tasks:[Task]) {
+        self.divisionType = divisionType
+        self.percentOfWhole = percentOfWhole
+        self.tasks = tasks
+    }
+    
+    // Getters
+    func getDivisionType() -> String {
+        return divisionType
+    }
+    
+    func getPercentOfWhole() -> Double {
+        return percentOfWhole
+    }
+    
+    func getTasks() -> [Task] {
+        return tasks
+    }
+    
+    func getNumberOfTasks() -> Int{
+        return tasks.count
+    }
+    
+    //Setters
+    func setDivisionType(divisionType:String) {
+        self.divisionType = divisionType
+    }
+    
+    func setPercentOfWhole(percentOfWhole:Double) {
+        self.percentOfWhole = percentOfWhole
+    }
+    
+    func setTasks(tasks:[Task]) {
+        self.tasks = tasks
+    }
+    
+    // Methods
+    func addTask(aTask:Task) {
+        tasks.append(aTask)
+    }
+    
+    // Methods to design
+    func removeTask(taskName:String) {
+        
+    }
 }
--- PlanX/Student/Student.swift
+++ PlanX/Student/Student.swift
@@ -7,11 +7,61 @@
 //
 
 import Foundation
+import FirebaseDatabase
+import FirebaseAuth
 
 class Student {
-    private var firstName:String = ""
-    private var lastName:String = ""
-    private var uid:String = ""
-    private var courses:[Course] = []
+    
+    private var firstName:String
+    private var lastName:String
+    private var uid:String          //unique UID of this user
+    private var courses:[Course]
+    
+    // Constructors
+    init(firstName:String, lastName:String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        uid = Auth.auth().currentUser!.uid
+        courses = []
+    }
+    
+    // Setters
+    func setFirstName(firstName:String) {
+        self.firstName = firstName
+    }
+    
+    func setLastName(lastName:String) {
+        self.lastName = lastName
+    }
+    
+    // Getters
+    func getName() -> String {
+        return firstName + lastName
+    }
+    
+    func getUID() -> String {
+        return uid
+    }
+    
+    // Methods to design:
+    func addCourse() {
+        
+    }
+    
+    func removeCourse() {
+        
+    }
+    
+    func addTaskToCourse() {
+        
+    }
+    
+    func removeTaskFromCourse() {
+        
+    }
+    
+    func editTaskOfCourse() {
+        
+    }
     
 }
