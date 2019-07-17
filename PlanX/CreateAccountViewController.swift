9f779ac571e9e6a2565f16551ef5ff764bdf45c4
diana sok <dianadesok98@gmail.com>
2019-07-17 12:37:05 -0700

    moveable keyboard

--- PlanX/CreateAccountViewController.swift
+++ PlanX/CreateAccountViewController.swift
@@ -33,7 +33,23 @@ class CreateAccountViewController: UIViewController {
      
      override func viewDidLoad() {
           super.viewDidLoad()
+          
+          NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+          NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
           //ref = Database.database().reference()
+     }
+     
+     @objc func keyboardWillShow(notification: NSNotification) {
+          if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+               if self.view.frame.origin.y == 0 {
+                    self.view.frame.origin.y -= keyboardSize.height
+               }
+          }
+     }
+     @objc func keyboardWillHide(notification: NSNotification) {
+          if self.view.frame.origin.y != 0 {
+               self.view.frame.origin.y = 0
+          }
      }
      
      override func didReceiveMemoryWarning() {
